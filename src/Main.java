88942f825a075e33d80ee8ced973c8e1fe5c0590
CAW-94 <carysannwilliams@outlook.com>
2024-06-30 13:25:55 +0100

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.math.BigDecimal;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //Set variables
@@ -40,8 +38,5 @@ public class Main {
         //Check balance
 
         oysterCard.checkBalance();
-
-
-
 }
 }
